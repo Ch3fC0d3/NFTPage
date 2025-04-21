e051ec3001387594d6c469066a82e87ca2890b85
Ch3fC0d3 <github@example.com>
2025-04-21 11:01:53 -0500

    Create simplified standalone application focused on Sepolia network

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,90 +2,62 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 
-module.exports = (env, argv) => {
-  const isProduction = argv.mode === 'production';
-  
-  return {
-    entry: './src/main.jsx',
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: 'bundle.[contenthash].js',
-      clean: true,
-      publicPath: '/',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react']
-            }
-          }
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader']
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        },
-        {
-          test: /\.json$/,
-          type: 'asset/source',
-          generator: {
-            dataUrl: content => {
-              return content;
-            }
+module.exports = {
+  mode: 'production',
+  entry: './src/main.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
-      ]
-    },
-    resolve: {
-      extensions: ['.js', '.jsx', '.json'],
-      fallback: {
-        "stream": require.resolve("stream-browserify"),
-        "buffer": require.resolve("buffer/"),
-        "os": require.resolve("os-browserify/browser"),
-        "path": require.resolve("path-browserify"),
-        "crypto": require.resolve("crypto-browserify"),
-        "http": require.resolve("stream-http"),
-        "https": require.resolve("https-browserify"),
-        "assert": require.resolve("assert/"),
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
       }
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './index.html',
-        filename: 'index.html',
-      }),
-      new webpack.ProvidePlugin({
-        Buffer: ['buffer', 'Buffer'],
-        process: 'process/browser',
-      }),
-      // Define plugin to provide global constants
-      new webpack.DefinePlugin({
-        'process.env.CONTRACT_ADDRESS_SEPOLIA': JSON.stringify('0xd9145CCE52D386f254917e481eB44e9943F39138'),
-        'process.env.CHAIN_ID_SEPOLIA': JSON.stringify('11155111'),
-      }),
-    ],
-    devServer: {
-      static: {
-        directory: path.join(__dirname, 'dist'),
-      },
-      compress: true,
-      port: 3000,
-      hot: true,
-      historyApiFallback: true,
-    },
-    devtool: isProduction ? 'source-map' : 'eval-source-map',
-    optimization: {
-      splitChunks: {
-        chunks: 'all',
-      },
-    },
-  };
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    fallback: {
+      "stream": require.resolve("stream-browserify"),
+      "buffer": require.resolve("buffer/"),
+      "process": require.resolve("process/browser"),
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      filename: 'index.html',
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+      process: 'process/browser',
+    }),
+    // Define plugin to provide contract address as global constant
+    new webpack.DefinePlugin({
+      'SEPOLIA_CONTRACT_ADDRESS': JSON.stringify('0xd9145CCE52D386f254917e481eB44e9943F39138'),
+      'SEPOLIA_CHAIN_ID': JSON.stringify('11155111'),
+    }),
+  ],
+  // Disable code splitting to avoid dynamic import issues
+  optimization: {
+    splitChunks: false,
+  },
 };
