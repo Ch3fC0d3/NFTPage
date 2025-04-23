fe71afdb22f8ae1b0d6e51c033c349376d63737f
Voices Ignited Admin <user@example.com>
2025-04-23 11:40:52 -0500

    Fix Digital Ocean deployment: simplify dependencies, add Procfile, disable package-lock

--- deploy/next.config.js
+++ deploy/next.config.js
@@ -6,14 +6,7 @@ const nextConfig = {
       ...config.resolve.fallback,
       fs: false,
       net: false,
-      tls: false,
-      crypto: require.resolve('crypto-browserify'),
-      stream: require.resolve('stream-browserify'),
-      http: require.resolve('stream-http'),
-      https: require.resolve('https-browserify'),
-      zlib: require.resolve('browserify-zlib'),
-      path: require.resolve('path-browserify'),
-      os: require.resolve('os-browserify/browser'),
+      tls: false
     };
     return config;
   },
